703f6d0a40d332cf28e63306a54385e3e7a9afdb
Ben Lehrburger <67259394+benlehrburger@users.noreply.github.com>
2023-05-23 22:27:05 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,70 @@
-# HCI-Frontend
-React.js tutorial deliverable + custom final project home page
+# Getting Started with Create React App
 
-## Description
-These code files comprise my deliverables for COSC 067 assignment A2.2. The [React.js tutorial](https://www.youtube.com/watch?v=hQAHSlTtcmY) deliverable can be found in the [tutorial folder](https://github.com/benlehrburger/HCI-Frontend/tree/main/react-tutorial) while the deliverable I created as an extension of the tutorial lives under the [custom implementation](https://github.com/benlehrburger/HCI-Frontend/tree/main/custom-implementation) directory.
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Troubleshooting
-I developed my program with VSCode and transferred it to GitHub ad hoc. I was not able to upload the <i>node_modules</i> folder that accompanies React web apps because it consisted of too many files to upload in a reasonable amount of time. If you run into trouble running the app, I would suggest pulling a <i>node_modules</i> folder from another web-app into each project's directory (both the tutorial and custom implementation).
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+
+If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+
+You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
---
+++ src/SignUp.jsx
@@ -0,0 +1,143 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import test from './img/test.png';
+import Login from './Login.jsx';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://www.intro2hci.site/project#h.iem6bgwlw52">
+        CalenDart
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function getRandomImage() {
+
+  var randomImage = new Array();
+
+  randomImage[0] = 'https://static.wikia.nocookie.net/nupepedia/images/2/27/Baker_Winter_Dartmouth_College.jpg/revision/latest?cb=20160429160929';
+  randomImage[1] = 'https://www.dartmouthcollegefund.org/sites/default/files/wide_12.jpg';
+  randomImage[2] = 'https://www.dartmouthcollegefund.org/sites/default/files/wide_10.jpg';
+  randomImage[3] = 'https://www.dartmouthcollegefund.org/sites/default/files/wide_8.jpg';
+  randomImage[4] = 'https://www.dartmouthcollegefund.org/sites/default/files/wide_9.jpg';
+  randomImage[5] = 'https://www.dartmouthcollegefund.org/sites/default/files/wide_3.jpg';
+  randomImage[6] = 'https://www.dartmouthcollegefund.org/sites/default/files/wide_1.jpg';
+  randomImage[7] = 'https://www.dartmouthcollegefund.org/sites/default/files/wide_4.jpg';
+  randomImage[8] = 'https://www.dartmouthcollegefund.org/sites/default/files/wide_11.jpg';
+  randomImage[9] = 'https://mediaim.expedia.com/destination/1/912cb9a5e8e758d0485de17146e3a447.jpg';
+  randomImage[10] = 'https://graduate.dartmouth.edu/sites/graduate_studies.prod/files/graduate_studies/images/dart-campus-spring-1_0.jpg';
+
+  var number = Math.floor(Math.random()*randomImage.length);
+  console.log(randomImage[number])
+  return randomImage[number];
+};
+
+const defaultTheme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <div style={{ paddingBottom: '20px' }}>
+              <img src="https://raw.githubusercontent.com/benlehrburger/HCI-Frontend/main/custom-implementation/src/logo.png" alt="CalenDART Logo" width='220' height='80' />
+            </div>
+            <Typography component="h1" variant="h6" sx={{ color: "#666666" }}>
+              Sign up with your Dartmouth NetID credentials
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, backgroundColor: "#027202" }}
+              >
+                Sign Up
+              </Button>
+              <Grid container alignItems="center" direction="column" justifyContent="center">
+                <Link href="#" variant="body2" color="inherit" sx={{ color: "#666666" }}>
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://graduate.dartmouth.edu/sites/graduate_studies.prod/files/graduate_studies/images/dart-campus-spring-1_0.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+      </Grid>
+    </ThemeProvider>
+  );
+}
